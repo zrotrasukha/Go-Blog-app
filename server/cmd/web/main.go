7a20367ac786c0dbca4e909fe9dcdf3b6f02b299
zrotrasukha <zrotrasukha@gmail.com>
2026-02-15 02:09:09 +0530

    Introduce flags to bring flexibility change runtime variables

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorlog *log.Logger
+	infolog  *log.Logger
+}
+
 func main() {
+
+	addr := flag.String("addr", ":8000", "HTTP networ address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
+
+	app := &application{
+		errorlog: errorLog,
+		infolog:  infoLog,
+	}
 
 	fileServer := http.FileServer(http.Dir("server/ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/health", health)
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/post/view", postView)
-	mux.HandleFunc("/post/create", postCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/post/view", app.postView)
+	mux.HandleFunc("/post/create", app.postCreate)
 
-	log.Println("server is starting at :8000")
-	err := http.ListenAndServe(":8000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("server is starting at %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
