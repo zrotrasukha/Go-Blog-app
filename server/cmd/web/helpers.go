39d293f6d044a66367ad7732bf86ed59eb30bf20
zrotrasukha <zrotrasukha@gmail.com>
2026-02-18 13:50:43 +0530

    Improve the error handling by writing the output to buffer first

--- server/cmd/web/helpers.go
+++ server/cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -28,10 +29,15 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
+
 }
