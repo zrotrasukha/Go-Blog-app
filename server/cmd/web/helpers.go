a7a6a28264dfc8b4e9468437c1dafa6f60d8d238
zrotrasukha <zrotrasukha@gmail.com>
2026-02-19 03:45:07 +0530

    Add newSnippetData function to provide current year for templates

--- server/cmd/web/helpers.go
+++ server/cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -39,5 +40,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 	w.WriteHeader(status)
 
 	buf.WriteTo(w)
+}
 
+func (app *application) newSnippetData() *templateData {
+	return &templateData{
+		CurrentYear: time.Now().Year(),
+	}
 }
