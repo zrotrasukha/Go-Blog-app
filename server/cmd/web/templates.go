7ff55ae3e8148d0434a2ef6a4fc6caba690398d2
zrotrasukha <zrotrasukha@gmail.com>
2026-02-18 13:47:03 +0530

    Fix the overwriting bug of template

--- server/cmd/web/templates.go
+++ server/cmd/web/templates.go
@@ -33,7 +33,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		ts, err = template.ParseFiles(page)
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
