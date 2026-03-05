e3b6c66892e271c94056ab83f265e0d46886c224
zrotrasukha <zrotrasukha@gmail.com>
2026-03-05 20:05:24 +0000

    Refactor template handling and embed static files for improved performance

--- server/cmd/web/routes.go
+++ server/cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"github.com/zrotrasukha/Go-Blog-app/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -14,8 +15,8 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 	// search about compatibility
-	fileServer := http.FileServer(http.Dir("ui/static"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 	router.HandlerFunc(http.MethodGet, "/health", health)
 
 	// unpropected
--- server/cmd/web/templates.go
+++ server/cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/zrotrasukha/Go-Blog-app/internal/models"
+	"github.com/zrotrasukha/Go-Blog-app/ui"
 )
 
 type templateData struct {
@@ -29,7 +31,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
@@ -37,17 +39,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("ui/html/pages/base.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/pages/partials/*.html",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("ui/html/pages/partials/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
