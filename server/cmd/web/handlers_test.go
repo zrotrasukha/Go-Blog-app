6bb83595a87e8c86e9e3a462391fb4c32c937035
zrotrasukha <zrotrasukha@gmail.com>
2026-03-06 08:58:55 +0000

    Refactor TestHealth function to simplify HTTP server testing and improve readability

--- server/cmd/web/handlers_test.go
+++ server/cmd/web/handlers_test.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/zrotrasukha/Go-Blog-app/internal/assert"
 )
 
 func TestHealth(t *testing.T) {
-	app := &application{
-		errorlog: log.New(io.Discard, "", 0),
-		infolog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/health")
-	if err != nil {
-		t.Fatal(err)
-	}
+	statusCode, _, body := ts.get(t, "/health")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
